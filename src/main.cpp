b7a65cbf16c59be6d59f60ae669a47d846013308
Tomoki Imai <tomo832@gmail.com>
2015-08-05 07:01:51 +0900

    Print processing time in us.

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,12 @@
 #include "lodepng.h"
 #include <cuda_runtime.h>
 #include "kernel.h"
+#include <sys/time.h>
 #include <functional>
+
+long time_diff_us(struct timeval st, struct timeval et){
+    return (et.tv_sec-st.tv_sec)*1000000+(et.tv_usec-st.tv_usec);
+}
 
 
 int main(int argc, char** argv){
@@ -27,8 +32,18 @@ int main(int argc, char** argv){
     unsigned char* input_image  = new unsigned char[in_image.size()];
     unsigned char* output_image = new unsigned char[in_image.size()];
     std::copy(in_image.begin(), in_image.end(), input_image);
+
+    struct timeval st;
+    struct timeval et;
+    gettimeofday(&st, NULL);
+
+    
     rgb_invert_in_cuda(output_image, input_image, width, height);
     // rgb_invert_in_cpu(output_image, input_image, width, height);
+
+    gettimeofday(&et, NULL);
+    long us = time_diff_us(st, et);
+    printf("%lu\n",us);
 
     std::vector<unsigned char> out_image(output_image, output_image+in_image.size());
     unsigned output_error = lodepng::encode(output_file, out_image, width, height);
